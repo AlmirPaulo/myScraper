2be662d10f6e3d6116d5cc3aff6247127e5f3df5
AlmirPaulo <ap.freelas@gmail.com>
2020-12-29 19:31:28 -0300

    update

--- myScraper.py
+++ myScraper.py
@@ -1,3 +1,5 @@
+#! env/bin/python3
+from openpyxl import Workbook
 from requests_html import HTMLSession
 import sqlite3, csv, xlsxwriter
 
@@ -5,24 +7,31 @@ import sqlite3, csv, xlsxwriter
 url = input('url: ')
 session = HTMLSession()
 req = session.get(url)
-# req.html.render()
+#For dynamic pages
+#req.html.render()
 
 #Scrap data
 while True:
     source = input('What to scrape?\n >>> ')
-    slot = input('Where to slot? \n >>>')
+    slot = input('Where to slot? \n >>> ')
     try:
         scrap = req.html.find(source)
-        with open(slot[0:10], 'w') as f:
-            for i in scrap:
-                f.write(i.text)
+        data = []
+        for i in scrap:
+            data.append(i.text)
+        with open('slots/'+slot[0:10], 'w') as f:
+            slot_writer = csv.writer(f) 
+            slot_writer.writerow(data)
             f.close() 
     
     except:
         scrap = req.html.xpath(source)
-        with open(slot[0:10], 'w') as f:
-            for i in scrap:
-                f.write(i.text)
+        data = []
+        for i in scrap:
+            data.append(i.text)
+        with open('slots/'+slot[0:10], 'w') as f:
+            slot_writer = csv.writer(f)
+            slot_writer.write(data)
             f.close()    
     ask = input('More source?\n >>> ')
     if ask == 'y':
@@ -36,16 +45,19 @@ if out == "1":
 
     col = 0
     row = 0
-
+    #wb = Workbook()
+    #ws = wb.active
     workbook = xlsxwriter.Workbook('Output.xlsx')
     worksheet = workbook.add_worksheet()
 
 elif out == "2":
-    pass
+    output = open('Output.csv', 'w')
+    writer = csv.writer(out)
+
 elif out == "3":
     with open('Output.txt', 'w') as f:
-        for i in data:
-            f.write(i.text)
+        for j in scrap:
+            f.write(j.text)
         f.close()        
 elif out == "4":
     pass
